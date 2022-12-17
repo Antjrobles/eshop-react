3927f7bbf3c30f86a6a55c32a5d0505fc1bcefa6
Antonio Robles <antjrobles@gmail.com>
2022-12-17 12:09:31 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// PAGES 
+import { Home, Contact } from "./pages";       //no need to specify index.js
+// COMPONENTS 
+import { Header, Footer } from "./components"; //no need to specify index.js
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/contact" element={ <Contact /> } />
+      </Routes>
+    <Footer />
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/orderHistory/OderHistory.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Admin.modules.scss";
+
+
+
+
+const OrderHistory = () => {
+  return (
+    <div>OrderHistory</div>
+  )
+}
+
+export default OrderHistory;
